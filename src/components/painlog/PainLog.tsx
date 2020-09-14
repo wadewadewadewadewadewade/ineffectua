07aa2b9f9fb3c75cacbb611de6f2ee2fbc186b28
wadewadewadewadewadewade <wade.wade.wade.wade.wade.wade@gmail.com>
2020-09-14 08:01:58 -0700

    Getting closer on PainLog location dragging

--- src/components/painlog/PainLog.tsx
+++ src/components/painlog/PainLog.tsx
@@ -17,6 +17,9 @@ import DataTypes from '../shared/DataTypes';
 import Slider from '@react-native-community/slider';
 import Medications from '../shared/Medications';
 
+type ScreenPosition = { x: number, y: number}
+type ScreenPercentage = { left: string, top: string}
+
 type NewPainLogLocationProps = {
   value?: PainLogLocation
   painlog: PainLogType,
@@ -107,15 +110,49 @@ const Location = ({
   value,
   figureDimensions,
   theme,
-  onPress
+  updateLocation
 }: {
   value: PainLogLocation,
   figureDimensions: { width: number, height: number },
   theme: ThemeState['theme'],
-  onPress: () => void
+  updateLocation: (loc: PainLogLocation) => void
 }) => {
-  const [location, setLocation] = React.useState(value.position);
-  const offset = location ? { left: location.x + '%', top: location.y + '%' } : {}
+  if (!value.position || figureDimensions.width < 1) {
+    console.error('position or figure dimensions missing', {value, figureDimensions})
+    return <View></View>
+  }
+  const percentToPixels = (loc: ScreenPosition) => {
+    return {
+      x: loc.x / 100 * figureDimensions.width,
+      y: loc.y / 100 * figureDimensions.height
+    }
+  }
+  const pixelsToPercent = (loc: ScreenPosition, supplyPercentSymbol = false): ScreenPosition | ScreenPercentage => {
+    if (supplyPercentSymbol) {
+      return {
+        left: (loc.x / figureDimensions.width * 100).toFixed(2).toString() + '%',
+        top: (loc.y / figureDimensions.height * 100).toFixed(2).toString() + '%',
+      }  
+    } else {
+      return {
+        x: loc.x / 100 * figureDimensions.width,
+        y: loc.y / 100 * figureDimensions.height
+      }
+    }
+  }
+  const panToPixels = (p: Animated.ValueXY): ScreenPosition => {
+    return {
+      x: (pan.x as any)._value,
+      y: (pan.y as any)._value,
+    }
+  }
+  const addLocations = (loc1: ScreenPosition, loc2: ScreenPosition): ScreenPosition => {
+    return {
+      x: loc1.x + loc2.x,
+      y: loc1.y + loc2.y
+    }
+  }
+  const [location, setLocation] = React.useState(percentToPixels(value.position));
   const adjustForDesktop = Platform.OS === 'web' ? { position: 'absolute', zIndex: 2, padding: 0 } : {};
   const adjustForDesktopIcon = Platform.OS === 'web' ? { padding: 0, width: '50px' } : {};
   const createTwoButtonAlert = (loc: PainLogLocation) =>
@@ -132,49 +169,41 @@ const Location = ({
       ],
       { cancelable: false }
     );
-    const point = new Animated.ValueXY()
+    const pan = React.useRef(new Animated.ValueXY({x:0,y:0})).current;
     const scale = new Animated.Value(1)
-    console.log({figureDimensions})
     const panResponder = React.useRef(
       PanResponder.create({
-        // Ask to be the responder:
-        onStartShouldSetPanResponder: () => true,
-        onStartShouldSetPanResponderCapture: () => true,
         onMoveShouldSetPanResponder: () => true,
-        onMoveShouldSetPanResponderCapture: () => true,
-  
-        onPanResponderGrant: (e: GestureResponderEvent, gestureState: PanResponderGestureState) => {
-          Animated.event([{ x: point.x, y: point.y }])({ x: gestureState.x0, y: gestureState.y0 })
-          setLocation({ x: gestureState.x0 / figureDimensions.width, y: gestureState.y0 / figureDimensions.height })
-          //console.log('grant', figureDimensions, gestureState.x0 / figureDimensions.width, gestureState.y0 / figureDimensions.height, location)
+        onPanResponderGrant: () => {
+          pan.setOffset({
+            x: (pan.x as any)._value, // typings isn't right, so force 
+            y: (pan.y as any)._value  // typescript to believe me using 'any'
+          });
           Animated.timing(
             scale,
             { toValue: 1.2, duration: 0, useNativeDriver: true }
           ).start();
-          return true;
         },
-        onPanResponderMove: (e: GestureResponderEvent, gestureState: PanResponderGestureState) => {
-          //Animated.event([{ x: point.x, y: point.y }])({ x: gestureState.moveX, y: gestureState.moveY })
-          setLocation({ x: gestureState.moveX / figureDimensions.width, y: gestureState.moveY / figureDimensions.height  })
-          //console.log('move', figureDimensions, gestureState.x0 / figureDimensions.width, gestureState.y0 / figureDimensions.height, location)
-        },
+        onPanResponderMove: Animated.event(
+          [
+            null,
+            { dx: pan.x, dy: pan.y }
+          ], {useNativeDriver: true}
+        ),
         onPanResponderTerminationRequest: () => true,
-        onPanResponderRelease: (e: GestureResponderEvent, gestureState: PanResponderGestureState) => {
-          setLocation({ x: gestureState.x0 / figureDimensions.width, y: gestureState.y0 / figureDimensions.height })
-          //console.log('release', figureDimensions, gestureState.x0 / figureDimensions.width, gestureState.y0 / figureDimensions.height, location)
-        },
-        onPanResponderTerminate: () => {
-          
-        },
-        onShouldBlockNativeResponder: (evt, gestureState) => {
-          // Returns whether this component should block native components from becoming the JS
-          // responder. Returns true by default. Is currently only supported on android.
-          return true;
+        onPanResponderRelease: () => {
+          pan.flattenOffset();
+          Animated.timing(
+            scale,
+            { toValue: 1, duration: 0, useNativeDriver: true }
+          ).start();
+          setLocation(addLocations(panToPixels(pan), location))
+          updateLocation({...value, position: (pixelsToPercent(location, true) as {x:number,y:number})})
         }
       })
     ).current;
   return (
-    <Animated.View style={{...styles.location, ...offset, ...adjustForDesktop, transform: [{ scale }]}}>
+    <Animated.View style={{...styles.location, ...pixelsToPercent(location, true), ...adjustForDesktop, transform: [{ translateX: pan.x }, { translateY: pan.y }, { scale }]}}>
       <View { ...panResponder.panHandlers}>
         <Svg
             fill={paperColors(theme).accent} style={{...styles.locationIcon, ...adjustForDesktopIcon}}
@@ -201,18 +230,15 @@ export const PainLog = ({
   addNewPainLocation
 }: Props) => {
   const [location, setLocation] = React.useState(emptyPainLogLocation);
-  const [figureFrontDimensions, setFigureFrontDimensions] = React.useState({width:-1,height:-1});
-  const [figureBackDimensions, setFigureBackDimensions] = React.useState({width:-1,height:-1});
-  const figureDimensions = { width: figureFrontDimensions.width + figureBackDimensions.width, height: figureFrontDimensions.height}
+  const [figureDimensions, setFigureDimensions] = React.useState({width:-1,height:-1});
   const painLogArray = firebaseDocumentToArray(painlog);
   const adjustForDesktop = Platform.OS === 'web' ? { paddingVertical: 0} : {};
   const addLocation = (e: GestureResponderEvent, isBack: boolean = false) => {
-    const { width, height } = isBack ? figureBackDimensions : figureFrontDimensions
     const newLocation: PainLogLocation = {
       created: new Date(),
       position: {
-        x: Math.round(e.nativeEvent.locationX / width * 10000) / 100,
-        y: Math.round(e.nativeEvent.locationY / height * 10000) / 100,
+        x: Math.round(e.nativeEvent.locationX / figureDimensions.width * 10000) / 100,
+        y: Math.round(e.nativeEvent.locationY / figureDimensions.height * 10000) / 100,
       }
     }
     addNewPainLocation(newLocation)
@@ -220,13 +246,16 @@ export const PainLog = ({
   return (
     <View>
       <ScrollView centerContent={true}>
-        {painLogArray.filter(p => p.active).map(p => <Location value={p} figureDimensions={figureDimensions} theme={theme} onPress={() => setLocation(p)}/>)}
-        <View style={styles.container}>
-          <View
-            onLayout={(e: LayoutChangeEvent) => setFigureFrontDimensions(e.nativeEvent.layout)}
+        {painLogArray.filter(p => p.active).map(p => <Location value={p} figureDimensions={figureDimensions} theme={theme} updateLocation={(p2) => setLocation(p2)}/>)}
+        <View
+            style={styles.container}
+            onLayout={(e: LayoutChangeEvent) => setFigureDimensions({
+              width: e.nativeEvent.layout.width * 2,
+              height: e.nativeEvent.layout.height
+            })}
             onTouchStart={(e) => addLocation(e)}
-            style={styles.touchablefigure}
-          >
+        >
+          <View style={styles.touchablefigure}>
             <Svg
                 style={{...styles.figure, ...adjustForDesktop}}
                 fill={paperColors(theme).text}
@@ -236,11 +265,7 @@ export const PainLog = ({
               <Path d="M683,495.5c-5.8-7.3-13.3-17-15.4-22.7c-3.5-9.8-6.1-41.2-6.1-41.2c-1.2-37.1-10.2-53.2-10.2-53.2c-15.7-25.2-18.6-71.9-18.6-71.9l-0.7-79c-5.5-53.9-45.3-54.3-45.3-54.3c-40.2-6-45.7-19-45.7-19c-8.5-12.2-3.6-35.7-3.6-35.7c7.1-5.8,9.8-21,9.8-21c11.7-8.9,11.2-22.2,5.8-22c-4.4,0.1-3.4-3.5-3.4-3.5C556.6,13,504.1,10,504.1,10h-8c0,0-52.6,3-45.2,62.1c0,0,1,3.6-3.4,3.5c-5.4-0.1-5.9,13,5.8,22c0,0,2.7,15.2,9.8,21c0,0,4.8,23.5-3.7,35.7c0,0-5.6,12.9-45.7,19c0,0-39.8,0.4-45.2,54.3l-0.7,79c0,0-2.9,46.8-18.6,71.9c0,0-9.1,16.1-10.2,53.2c0,0-2.6,31.4-6.2,41.2c-2.1,5.7-9.6,15.5-15.4,22.7c-11.4,14.3-30.6,44-20.4,47.3c0,0,8,0.7,18.6-20.2c0,0-0.2,8.1-8.7,31.5c-1.6,4.4-8.7,26.5,2.7,18.6c0,0,5.3-3.7,12.2-26.5c0,0-3.7,38.4,0.4,40.4c5.4,2.6,8.3-4.9,10.8-38.7c0,0,2.5-10.8,3.6,30.7c0.1,2.1,3.3,12.8,7.6,3.8c3.7-7.7,2-27.9,2-34.7c0,0,4.7,26.2,9.1,26.2c0,0,5.2,6.1,3.1-26.4c-0.4-5.3,1.4-16.3,1.7-19.5l0.5-12.6c0,0-1.3-14.3-1.3-20.3c0-1.5,5.1-21,18.6-41.5c0,0,28.1-49.8,26.3-82.4c0,0-0.4-31.1,10.9-48.6c0,0,8,88,2.6,112.7c0,0-25.3,60.6-19.7,105.9c4.1,33.7,12.1,105.7,20.1,134.1c4.2,14.6,1.7,51.8,5.1,61c1.5,4,0.7,7.7-2.6,16.9c-11.4,32-9.9,54.3,18.6,138.9c0,0,8.8,18.7,4.4,52.4c0,0-18.2,37.4-6.5,38.2c0,0,1,2.5,4.9,0.5c0,0,6.3,6.5,13.1,3c0,0,6.3,5,11.7,0.5c0,0,4.4,4.9,10.7,1c0,0,8.3,5.5,13.2-0.6c0,0,8.8,2-6.8-37.5c0,0-6-41.7-9.3-49.9c-6.2-15.7-1.8-58.2-0.5-67.4c2.2-15.3,1-41.3-2.9-61.5c-2.8-14.4,4.9-41.7,7.8-58.4c6-35.2,17.5-125.3,16.1-142.2l4.8,1.6c3.5,0,5.7-1.6,5.7-1.6c-1.4,17,10,107,16,142.2c2.8,16.7,10.6,44,7.8,58.4c-4,20.1-5.2,46.2-3,61.5c1.4,9.1,5.8,51.7-0.5,67.4c-3.3,8.2-9.2,49.9-9.2,49.9c-15.6,39.4-6.8,37.5-6.8,37.5c4.9,6.1,13.1,0.6,13.1,0.6c6.3,3.9,10.7-1,10.7-1c5.4,4.5,11.6-0.5,11.6-0.5c6.8,3.5,13.1-3,13.1-3c3.9,2,4.8-0.5,4.8-0.5c11.7-0.7-6.5-38.2-6.5-38.2c-4.4-33.6,4.4-52.4,4.4-52.4c28.5-84.6,29.9-106.9,18.6-138.9c-3.3-9.1-4-13-2.6-16.9c3.4-9.2,0.9-46.4,5-61c8-28.4,16-100.4,20.1-134.1c5.5-45.3-19.7-105.9-19.7-105.9c-5.5-24.7,2.5-112.7,2.5-112.7c11.4,17.5,10.9,48.6,10.9,48.6c-1.9,32.5,26.3,82.4,26.3,82.4c13.5,20.6,18.6,40.1,18.6,41.5c0,6-1.3,20.3-1.3,20.3l0.5,12.5c0.2,3.2,2,14.2,1.7,19.5c-2,32.5,3.1,26.4,3.1,26.4c4.4,0,9.1-26.2,9.1-26.2c0,6.8-1.6,27.1,2,34.7c4.4,9,7.5-1.6,7.6-3.8c1.2-41.5,3.6-30.6,3.6-30.6c2.4,33.7,5.4,41.3,10.8,38.7c4.1-2,0.4-40.4,0.4-40.4c6.9,22.8,12.2,26.5,12.2,26.5c11.5,7.9,4.3-14.3,2.7-18.6c-8.5-23.4-8.8-31.5-8.8-31.5c10.6,21,18.5,20.2,18.5,20.2C713.6,539.5,694.3,509.8,683,495.5z M526,98.5c-5.7,0.4-5,10.9-5,10.9c-0.9,8.2-4.5,8.2-4.5,8.2c-3.4,0.3-3.3-5-3.3-5c-0.1-5-0.9-5.6-0.9-5.6l-2.2,6.8c-0.7,5.2-4.6,4-4.6,4l-3.4-10.4l-2.3,4c-1,7.5-3.4,6.1-3.4,6.1c-4.4,0.4-3.7-6.4-3.7-6.4c0.1-5.9-2.4-3.4-2.4-3.4c-0.4,9.8-2.7,9.9-2.7,9.9c-4.5,0.8-4.9-4.4-4.9-4.4c-1.2-14.7-4.6-14.4-4.6-14.4l-11.2-1.1c-8.3-3.3-5.7-12.1-3.1-14.8c3.2-3.4,2.7-8.2,2.7-8.2c-5.3-16.3-1.8-28.1-1.8-28.1c7.1-23.1,25.5-26.2,36.8-26c12.5,0.2,18.3,3,23.3,6.3c15.7,10.1,16.2,30.4,13.4,40.7c-1.9,6.9-0.3,13.3-0.3,13.3c5.6,6.7,3.4,11.9,3.4,11.9C539.3,100.6,526,98.5,526,98.5z"/>
             </Svg>
           </View>
-          <View
-              onLayout={(e: LayoutChangeEvent) => setFigureBackDimensions(e.nativeEvent.layout)}
-              onTouchStart={(e) => addLocation(e, true)}
-              style={styles.touchablefigure}
-          >
+          <View style={styles.touchablefigure}>
             <Svg
                 style={{...styles.figure, ...adjustForDesktop}}
                 fill={paperColors(theme).text}
