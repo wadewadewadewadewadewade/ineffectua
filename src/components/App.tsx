c3ec58b4d96267493943b4da150ce7b7259891e1
Wade McDaniel <wade.wade.wade.wade.wade.wade@gmail.com>
2020-11-02 16:03:53 -0800

    Despit not needing push, set up expo push to get notifications

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {YellowBox, Platform} from 'react-native';
+import {Subscription} from '@unimodules/react-native-adapter';
 import {PersistGate} from 'redux-persist/integration/react';
 import {Provider as ReduxProvider} from 'react-redux';
 import {store, persistor} from '../Store';
@@ -7,7 +8,9 @@ import {enableScreens} from 'react-native-screens';
 import Navigation from './Navigation';
 import {QueryCache, ReactQueryCacheProvider} from 'react-query'; // https://react-query.tanstack.com/docs/guides/suspense
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import Constants from 'expo-constants';
 import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 enableScreens();
 
 const queryCache = new QueryCache({
@@ -20,26 +23,90 @@ const queryCache = new QueryCache({
   },
 });
 
-export default function App() {
-  YellowBox.ignoreWarnings(['Setting a timer']); // Firebase uses long timers
-  if (Platform.OS !== 'web') {
-    // https://docs.expo.io/versions/latest/sdk/notifications/
-    Notifications.setNotificationChannelAsync('contacts', {
-      name: 'Emergency Contacts',
-      importance: Notifications.AndroidImportance.HIGH,
-    });
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
-    Notifications.scheduleNotificationAsync({
-      identifier: 'contacts',
-      content: {
-        title: 'Person',
-        body: 'Type: Name\nNumber',
-      },
-      trigger: {
-        seconds: 2,
-      },
+// Can use this function below, OR use Expo's Push Notification Tool-> https://expo.io/notifications
+async function sendPushNotification(expoPushToken: string) {
+  const message = {
+    to: expoPushToken,
+    sound: 'default',
+    title: 'Original Title',
+    body: 'And here is the body!',
+    data: { data: 'goes here' },
+  };
+
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      console.error('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    console.error('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
     });
   }
+
+  return token;
+}
+
+export default function App() {
+  const [expoPushToken, setExpoPushToken] = React.useState<string | undefined>('');
+  const [notification, setNotification] = React.useState<Notifications.Notification>();
+  let notificationListener: Subscription = {remove: () => {}};
+  let responseListener: Subscription =  {remove: () => {}};
+  YellowBox.ignoreWarnings(['Setting a timer']); // Firebase uses long timers
+  React.useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
   return (
     <ReduxProvider store={store}>
       <PersistGate loading={null} persistor={persistor}>
